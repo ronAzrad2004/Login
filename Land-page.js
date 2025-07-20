c5e26ea57d5b3d52f0e170fed6b32fc98be511bb
ronAzrad2004 <ron12azrad@gmail.com>
2025-07-20 14:37:59 +0300

    Update Land-page.js

--- Land-page.js
+++ Land-page.js
@@ -19,7 +19,7 @@ const blockedChars = ['<', '>', '"', "'", ';'];
     };
 
     try {
-      const res = await fetch("http://localhost:3000/contact", {
+      const res = await fetch("https://ronsapi.onrender.com/contact", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(data)
